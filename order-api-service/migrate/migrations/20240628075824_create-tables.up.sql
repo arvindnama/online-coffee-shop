86120d8d2787d1cfa6333b4d90c768fbe3fb4290
Arvind Nama <arvind.nu@gmail.com>
2024-06-28 10:04:05 -0700

    [go.microservice] migrations set up for order-api service

---
+++ order-api-service/migrate/migrations/20240628075824_create-tables.up.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS orders (
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(225) NOT NULL,
+    `totalPrice` FLOAT NOT NULL, 
+    `status` VARCHAR(35) NOT NULL,
+
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS products(
+    `id` INT UNSIGNED NOT NULL,
+    `order_id` INT UNSIGNED NOT NULL,
+    `name` VARCHAR(225) NOT NULL,
+    `quantity` INT NOT NULL,
+    `unit_price` FLOAT NOT NULL,
+
+    PRIMARY KEY(id),
+    FOREIGN KEY(order_id) REFERENCES orders(id)
+);
